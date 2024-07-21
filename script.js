0a4876cf1e558c83b756573a4aa6e1eb4d3b4713
Camilo <172856001+cub16@users.noreply.github.com>
2024-07-21 10:21:51 -0600

    Init

---
+++ script.js
@@ -0,0 +1,19 @@
+const projects = [
+  {
+    icon: "🌃",
+    name: "Lumina",
+    desc: "Lightweight notes",
+    langs: ["HTML", "CSS", "JS"],
+    url: "https://cub16.github.io/Lumina",
+  },
+];
+
+projects.forEach((project) => {
+  document.querySelector("#projects").innerHTML += `
+    <div class="project" onclick="location.href='${project.url}'">
+        <h3>${project.name} ${project.icon}</h3>
+        <p>${project.desc}</p>
+        <p>${project.langs}</p>
+    </div>
+  `;
+});
